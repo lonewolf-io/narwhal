4bd460d2e705d322844ded2547f29afbe508a96c
Miguel Ángel Ortuño <ortuman@gmail.com>
2026-03-24 13:32:41 +0100

    use runtime abstraction in modulator crate
    Signed-off-by: Miguel Ángel Ortuño <ortuman@gmail.com>

--- crates/modulator/src/conn.rs
+++ crates/modulator/src/conn.rs
@@ -13,6 +13,7 @@ use futures::FutureExt;
 use tracing::{error, trace, warn};
 
 use narwhal_common::conn::{ConnTx, State};
+use narwhal_common::runtime;
 use narwhal_common::service::{M2sService, S2mService};
 use narwhal_protocol::ErrorReason::{BadRequest, Unauthorized, UnexpectedMessage, UnsupportedProtocolVersion};
 use narwhal_protocol::{Event, Nid, S2mModDirectAckParameters};
@@ -318,7 +319,7 @@ pub struct S2mDispatcherFactoryInner<M: Modulator> {
   m2s_client: Option<M2sClient>,
 
   /// Handle to the private payload reader task.
-  payload_reader_handle: Option<monoio::task::JoinHandle<()>>,
+  payload_reader_handle: Option<runtime::JoinHandle<()>>,
 
   /// Shutdown sender for graceful shutdown.
   shutdown_tx: async_channel::Sender<()>,
@@ -417,7 +418,7 @@ impl<M: Modulator> narwhal_common::conn::DispatcherFactory<S2mDispatcher<M>> for
       let rx = response.receiver;
       let shutdown_rx = inner.shutdown_rx.clone();
 
-      let handle = monoio::spawn(Self::payload_reader_loop(rx, m2s_client.clone(), shutdown_rx));
+      let handle = runtime::spawn(Self::payload_reader_loop(rx, m2s_client.clone(), shutdown_rx));
       inner.payload_reader_handle = Some(handle);
     }
     Ok(())
